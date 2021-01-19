aeda6c78cda7e2b98b12d0572b50471bf22781f6
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-01-19 18:28:43 -0500

    Removed bug-comma in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
-    validates :password_digest, presence: true,
+    validates :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true}
     validates :email, presence: true
 
