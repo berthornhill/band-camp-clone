8ec28b397da15da611d10b1e9da5880561c04db1
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-01-20 10:37:16 -0500

    Fixed bugs in react component render for Root, App. Also fixed fixed .gitignore to properly exclude bundle.js files

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import SessionContainer from "./session/session_container";
+// import SessionContainer from "./session/session_container";
 
 const App = () => {
   return (
     <div>
-      <h1>Bandlcamp: Shining a light on your favorite music.</h1>;
+      <h1>Bandlcamp: Shining a light on your favorite music.</h1>
     </div>
   );
 };
